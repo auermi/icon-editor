757150f3367e1e7c3d7bd7c6cc6a3ec88d4ef9ee
Michael Auer <michael@michael-auer.co>
2016-03-02 13:03:15 -0500

    Apply fills to svgs based on user input. Commented a lot of code

--- renderer.js
+++ renderer.js
@@ -17,28 +17,47 @@ var menu = Menu.buildFromTemplate([
 ])
 Menu.setApplicationMenu(menu)
 
+// DOM Refernces
 const logoSelect = document.getElementById('logoSelect')
+const logoColor = document.getElementById('logoColor')
+const backgroundColor = document.getElementById('backgroundColor')
+const colorButton = document.getElementById('colorButton')
 const logo = document.getElementById('logo')
 
 // Generate menu based on available icons
 const logos = fs.readdirSync(__dirname + '/logos').filter((x) => {
+  // Don't want system files
   return x.charAt(0) !== '.'
 }).map((x) => {
+  // Clean names without file extension
   return x.replace('.svg', '')
 }).forEach((x) => {
+  // Append an option to the select element
   const node = document.createElement('OPTION')
   node.appendChild(document.createTextNode(x))
   logoSelect.appendChild(node)
 })
 
+// When a new logo is selected, inject it
 logoSelect.addEventListener('change', () => {
-  if (logoSelect.value === logoSelect.children[0].innerText) {
-    logo.innerHTML = ''
-    return
-  }
   const uri = __dirname + '/logos/' + logoSelect.value + '.svg'
   logo.innerHTML = fs.readFileSync(uri, 'utf-8', (err,data) => {
     if (err) throw error
     return data
   })
 })
+
+// Apply fill colors to svg
+var applyStyles = () => {
+  return () => {
+    // Should disable fill buttons eventually but this works for now
+    if (logoSelect.value === logoSelect.children[0].value) {
+      return
+    }
+    document.querySelector('#logo path').style.fill = '#' + logoColor.value
+    document.querySelector('#logo rect').style.fill = '#' + backgroundColor.value
+  }
+}
+
+// Apply styles on click
+colorButton.addEventListener('click', applyStyles())
