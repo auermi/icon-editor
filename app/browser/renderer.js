a1dce5520996670699e7db1eba68ed2abf7755e7
Michael Auer <michael@michael-auer.co>
2016-03-03 15:36:58 -0500

    Add isCircle option

--- app/browser/renderer.js
+++ app/browser/renderer.js
@@ -10,6 +10,8 @@ menu.init()
 const logoSelect = document.getElementById('logoSelect')
 const logoColor = document.getElementById('logoColor')
 const backgroundColor = document.getElementById('backgroundColor')
+const backgroundIsActive = document.getElementById('backgroundIsActive')
+const backgroundIsCircle = document.getElementById('backgroundIsCircle')
 const colorButton = document.getElementById('colorButton')
 const saveButton = document.getElementById('saveButton')
 const logo = document.getElementById('logo')
@@ -42,22 +44,40 @@ logoSelect.addEventListener('change', () => {
 // Apply fill colors to svg
 var applyStyles = () => {
   return () => {
+    const circle = document.querySelector('#logo circle')
+    const rect = document.querySelector('#logo rect')
+    const shape = document.querySelector('#logo path')
+
     // Should disable fill buttons eventually but this works for now
     if (logoSelect.value === logoSelect.children[0].value) {
       return
     }
 
-    // Reference bg rect and shape path
-    const shape = document.querySelector('#logo path')
-    const rect = document.querySelector('#logo rect')
+    // is bg
+    if (backgroundIsActive.checked) {
+      // is circle
+      if (backgroundIsCircle.checked) {
+        circle.style.opacity = 1
+        rect.style.opacity = 0
+        circle.style.fill = '#' + backgroundColor.value
+      // is rect
+      } else {
+        circle.style.opacity = 0
+        rect.style.opacity = 1
+        rect.style.fill = '#' + backgroundColor.value
+        // Border Radius
+        rect.setAttribute('rx', radius.value)
+      }
+    // no bg
+    } else {
+      rect.style.opacity = 0
+      circle.style.opacity = 0
+    }
 
-    // Color fills
+    // Fill logo
     shape.style.fill = '#' + logoColor.value
-    rect.style.fill = '#' + backgroundColor.value
-
-    // Border Radius
-    rect.setAttribute('rx', radius.value)
-    rect.setAttribute('ry', radius.value)
+    
+    // Radius label is equal to slider value
     radiusLabel.innerText = radius.value
   }
 }
