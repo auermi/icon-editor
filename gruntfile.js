37e13a9c2e4798a6cfc60a1663b9199d234f554e
Michael Auer <michael@michael-auer.co>
2016-03-04 14:39:35 -0500

    Update gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -28,5 +28,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass')
     grunt.loadNpmTasks('grunt-contrib-cssmin')
     grunt.loadNpmTasks('grunt-contrib-watch')
-    grunt.registerTask('default', ['watch'])
+    grunt.registerTask('watch', 'watch')
+    grunt.registerTask('default', ['sass', 'cssmin'])
 }
