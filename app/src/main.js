c2a22d1e9a18e7b7c1222610e2be5be9a91ee085
Michael Auer <michael@michael-auer.co>
2016-03-04 11:36:45 -0500

    Move save to its own module

--- app/src/main.js
+++ app/src/main.js
@@ -5,8 +5,7 @@ const app = electron.app
 const BrowserWindow = require('browser-window')
 const dialog = electron.dialog
 const ipcMain = electron.ipcMain
-const fs = require('fs')
-const gm = require('gm')
+const Save = require('./save.js')
 
 app.on('ready', () => {
   // New Browser Window
@@ -22,49 +21,7 @@ app.on('ready', () => {
 
   // Renderer Listeners
   ipcMain.on('save', (event, arg) => {
-    const svg = arg
-    // If we have an svg
-    if (svg !== '') {
-      // Save pop up
-      dialog.showSaveDialog((fileName) => {
-        // If we have filename
-        if (fileName) {
-          // Does a file with the same name exist already?
-          if (fs.readFile(fileName + '.png', (err, data) => { return err }) !== null) {
-            // It exists, show pop up -> if yes continue overrwrite, if no, cancel
-            dialog.showMessageBox({
-              message: 'File with name: ' + fileName + '.png already exists',
-              detail: 'Do you want to overwrite it?',
-              buttons: ['Yes', 'No']
-            }, (response) => {
-              if (response === 1) {
-                return
-              } else {
-                // Write to SVG
-                const localuri = __dirname + '/../.cache/icon.svg'
-                fs.writeFileSync(localuri, svg)
-                // Write to PNG fom SVG
-                gm(localuri)
-                .write(fileName + '.png', (err) => {
-                  if (err) throw (err)
-                  // Delete the SVG
-                  fs.unlink(localuri)
-                })
-              }
-            })
-          }
-        } else {
-          return // Hit cancel on save dialog
-        }
-      })
-    } else {
-      // No SVG on DOM
-      dialog.showMessageBox({
-        message: 'No Icon',
-        detail: 'Add an icon by selecting one from the dropdown menu',
-        buttons: ['Ok']
-      })
-    }
+    Save.save(arg)
   })
   ipcMain.on('about', () => {
     const date = new Date()
