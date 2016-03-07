a2c64d61b24b507e75cf943a26233c99124ee13b
Michael Auer <michael@michael-auer.co>
2016-03-07 11:43:59 -0500

    Move styling code to its own module

--- app/browser/renderer.js
+++ app/browser/renderer.js
@@ -2,6 +2,7 @@
 
 const menu = require('./menu.js')
 const ipcRenderer = require('electron').ipcRenderer
+const styles = require('./styles.js')
 
 // Initialize Menu
 menu.init()
@@ -32,65 +33,9 @@ logoSelect.addEventListener('change', () => {
   ipcRenderer.send('getSVG', logoSelect.value)
   ipcRenderer.on('sendLogoSVG', (event, message) => {
     logo.innerHTML = message
-    applyStyles()
+    styles.apply()
   })
 })
 
-// Apply fill colors to svg
-const applyStyles = () => {
-    const circle = document.querySelector('#logo circle')
-    const rect = document.querySelector('#logo rect')
-    const shape = document.querySelector('#logo path')
-
-    // Should disable fill buttons eventually but this works for now
-    if (logoSelect.value === logoSelect.children[0].value) {
-      return
-    }
-    // is bg
-    if (backgroundIsActive.checked) {
-      // is circle
-      if (backgroundIsCircle.checked) {
-        circle.style.opacity = 1
-        rect.style.opacity = 0
-        isValidHex(backgroundColor.value)
-          ? circle.setAttribute('fill', '#' + backgroundColor.value)
-          : circle.setAttribute('fill', '#000000')
-      // is rect
-      } else {
-        circle.style.opacity = 0
-        rect.style.opacity = 1
-        isValidHex(backgroundColor.value)
-          ? rect.setAttribute('fill', '#' + backgroundColor.value)
-          : rect.setAttribute('fill', '#000000')
-        // Border Radius
-        rect.setAttribute('rx', radius.value)
-      }
-    // no bg
-    } else {
-      rect.style.opacity = 0
-      circle.style.opacity = 0
-    }
-
-    // Fill logo
-    isValidHex(logoColor.value)
-      ? shape.setAttribute('fill', '#' + logoColor.value)
-      : shape.setAttribute('fill', '#FFFFFF')
-
-    // Radius label is equal to slider value
-    radiusLabel.innerText = radius.value
-}
-
 // Apply styles on click
-colorButton.addEventListener('click', applyStyles)
-
-const isValidHex = (x) => {
-  // If x isnt null
-  if (x) {
-    // validate
-    let re = new RegExp('[0-9a-f]{6}', 'i')
-    if (x.length === 6 && re.test(x)) {
-      return true
-    }
-  }
-  return false
-}
+colorButton.addEventListener('click', styles.apply)
---
+++ app/browser/styles.js
@@ -0,0 +1,59 @@
+'use strict'
+
+// Apply fill colors to svg
+const apply = () => {
+    const circle = document.querySelector('#logo circle')
+    const rect = document.querySelector('#logo rect')
+    const shape = document.querySelector('#logo path')
+
+    // Should disable fill buttons eventually but this works for now
+    if (logoSelect.value === logoSelect.children[0].value) {
+      return
+    }
+    // is bg
+    if (backgroundIsActive.checked) {
+      // is circle
+      if (backgroundIsCircle.checked) {
+        circle.style.opacity = 1
+        rect.style.opacity = 0
+        isValidHex(backgroundColor.value)
+          ? circle.setAttribute('fill', '#' + backgroundColor.value)
+          : circle.setAttribute('fill', '#000000')
+      // is rect
+      } else {
+        circle.style.opacity = 0
+        rect.style.opacity = 1
+        isValidHex(backgroundColor.value)
+          ? rect.setAttribute('fill', '#' + backgroundColor.value)
+          : rect.setAttribute('fill', '#000000')
+        // Border Radius
+        rect.setAttribute('rx', radius.value)
+      }
+    // no bg
+    } else {
+      rect.style.opacity = 0
+      circle.style.opacity = 0
+    }
+
+    // Fill logo
+    isValidHex(logoColor.value)
+      ? shape.setAttribute('fill', '#' + logoColor.value)
+      : shape.setAttribute('fill', '#FFFFFF')
+
+    // Radius label is equal to slider value
+    radiusLabel.innerText = radius.value
+}
+
+const isValidHex = (x) => {
+  // If x isnt null
+  if (x) {
+    // validate
+    let re = new RegExp('[0-9a-f]{6}', 'i')
+    if (x.length === 6 && re.test(x)) {
+      return true
+    }
+  }
+  return false
+}
+
+exports.apply = apply
